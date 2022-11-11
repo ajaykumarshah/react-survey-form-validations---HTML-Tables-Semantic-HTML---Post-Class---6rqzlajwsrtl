e1ff1bfca9ddcf5a634548caf4364268aeb8dbcc
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-12 00:44:40 +0530

    j

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import './styles/App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [userdetails, setUserdetails] = useState({ firstname: "", lastname: "", username: "", email: "", contact: "", postalcode: "" });
+  const handlechange = (event) => {
+    setUserdetails({ ...userdetails, [event.target.name]: event.target.value });
+
+  }
+
+  const changehandler = () => {
+    if (userdetails.firstname.indexOf(" ") > 0) {
+      alert("Enter valid first name!");
+      return;
+    }
+    else if (userdetails.lastname.indexOf(" ") > 0) {
+      alert("Enter valid last name!");
+    }
+    else if (! /[^0-9a-zA-Z]/.test((userdetails.username))) {
+      alert("Enter valid username!");
+    }
+    else if (!  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(userdetails.email)) {
+      alert("Enter valid email!");
+    }
+    else if (!(userdetails.contact.match('[0-9]{10}'))) {
+      alert("Enter valid contact!");
+    }
+    else if (!userdetails.postalcode.length == 6 && typeof (userdetails.postalcode) == number) {
+      alert("Enter valid postal code!");
+    }
+    else {
+      alert("Submitted");
+    }
+  }
   return (
     <div className="container">
       <h1>Survey From</h1>
@@ -10,66 +42,66 @@ function App() {
       <div className="form row g-3">
         <h5>Personal Details</h5>
         <div className="col-md-6">
-          <label for="validationDefault01" className="form-label">
+          <label htmlFor="validationDefault01" className="form-label">
             First name
           </label>
-          <input type="text" className="form-control" name="first-name" id="first-name" placeholder="First Name" />
+          <input type="text" className="form-control" onChange={handlechange} value={userdetails.firstname} name="firstname" id="first-name" placeholder="First Name" />
         </div>
         <div className="col-md-6">
-          <label for="validationDefault02" className="form-label">
+          <label htmlFor="validationDefault02" className="form-label">
             Last name
           </label>
-          <input type="text" className="form-control" name="last-name" id="last-name" placeholder="Last Name" />
+          <input type="text" className="form-control" onChange={handlechange} value={userdetails.lastname} name="lastname" id="lastname" placeholder="Last Name" />
         </div>
         <div className="col-md-6">
-          <label for="validationDefaultUsername" className="form-label">
+          <label htmlFor="validationDefaultUsername" className="form-label">
             Username
           </label>
           <div className="input-group">
             <span className="input-group-text" id="inputGroupPrepend2">
               @
             </span>
-            <input type="text" className="form-control" name="username" id="username" aria-describedby="inputGroupPrepend2" placeholder="user1234" />
+            <input type="text" className="form-control" onChange={handlechange} value={userdetails.username} name="username" id="username" aria-describedby="inputGroupPrepend2" placeholder="user1234" />
           </div>
         </div>
         <div className="col-md-6">
-          <label for="validationDefault03" className="form-label">
+          <label htmlFor="validationDefault03" className="form-label">
             Email
           </label>
-          <input type="email" className="form-control" id="email" name="email" placeholder="user@email.com" />
+          <input type="email" className="form-control" onChange={handlechange} id="email" value={userdetails.email} name="email" placeholder="user@email.com" />
         </div>
         <div className="col-md-6">
-          <label for="validationDefault03" className="form-label">
+          <label htmlFor="validationDefault03" className="form-label">
             Contact No
           </label>
-          <input type="text" className="form-control" id="contact" name="contact" placeholder="xxx-xxx-xxxx" />
+          <input type="text" className="form-control" onChange={handlechange} id="contact" value={userdetails.contact} name="contact" placeholder="xxx-xxx-xxxx" />
         </div>
         <div className="col-md-6">
-          <label for="validationDefault05" className="form-label">
+          <label htmlFor="validationDefault05" className="form-label">
             Poctal Code / Zip
           </label>
-          <input type="text" className="form-control" id="postal-code" name="postal-code" placeholder="xx-xx-xx" />
+          <input type="text" className="form-control" onChange={handlechange} id="postal-code" value={userdetails.postalcode} name="postalcode" placeholder="xx-xx-xx" />
         </div>
         <div className="col-md-12">
           <h5>Would you recommend Newton School to a friend?</h5>
 
           <div className="form-check">
             <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-            <label className="form-check-label" for="flexRadioDefault1">
+            <label className="form-check-label" htmlFor="flexRadioDefault1">
               {' '}
               Default radio{' '}
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-            <label className="form-check-label" for="flexRadioDefault2">
+            <label className="form-check-label" htmlFor="flexRadioDefault2">
               {' '}
               Default checked radio{' '}
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-            <label className="form-check-label" for="flexRadioDefault2">
+            <label className="form-check-label" htmlFor="flexRadioDefault2">
               {' '}
               Default checked radio{' '}
             </label>
@@ -81,68 +113,68 @@ function App() {
           </h5>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               C
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               C++
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               Java
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               Python
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               Javascript
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               React
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               Angular
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               Django
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               Spring
             </label>
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" id="invalidCheck2" />
-            <label className="form-check-label" for="invalidCheck2">
+            <label className="form-check-label" htmlFor="invalidCheck2">
               Android
             </label>
           </div>
         </div>
 
         <div className="col-12">
-          <button className="btn btn-primary" type="submit">
+          <button className="btn btn-primary" type="submit" onClick={changehandler}  >
             Submit form
           </button>
         </div>
